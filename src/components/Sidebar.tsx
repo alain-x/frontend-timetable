e42cf4137dc1b2f8c7e7a6d8683c3990ddec0cfc
alain-x <alainvalentin04@gmail.com>
2026-03-18 16:30:38 +0200

    initial commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,6 +16,7 @@ const getRoleLinks = (role: string) => {
       { id: 'overview', label: 'Overview', icon: 'M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2-2z' },
       { id: 'calendar', label: 'Timetable', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
       { id: 'all-timetables', label: 'All Timetables', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16' },
+       { id: 'all-timetables', label: 'All Timetables', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16' },
       { id: 'announcements', label: 'Announcements', icon: 'M13 16h-1v-4h-1m4-4V4a2 2 0 00-2-2H7a2 2 0 00-2 2v16a2 2 0 002 2h6a2 2 0 002-2v-4m-4 0h4' },
       { id: 'swap-requests', label: 'Swap Requests', icon: 'M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4' },
       { id: 'course-completion', label: 'Course Completion', icon: 'M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4' },
